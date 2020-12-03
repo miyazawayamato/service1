0937a887e9902104b1b65e8b8ff38733a1361917
miyazawayamato <yamato8781@gmail.com>
2020-12-03 11:34:04 +0900

    メール認証

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,8 @@
 namespace App\Models;
 
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;//メール認証
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -12,7 +13,7 @@ use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
 
 
-class User extends Authenticatable //implements MustVerifyEmail
+class User extends Authenticatable implements MustVerifyEmail //im～mailまで追加 認証のため
 {
     use HasApiTokens;
     use HasFactory;
